e6ef614f8ba9054255e86c087acbf3534c7eccd8
tassaddar <tassaddar-90@yandex.ru>
2017-05-11 22:10:34 +0300

    Added book titles task

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,23 @@
 class Book
 # write your code here
+attr_accessor :title
+
+@title = ''
+
+def title=(titles)
+	
+	split_titles = titles.split(" ")
+	split_titles = [split_titles[0].capitalize] + 
+	split_titles[1..-1].map do |word|
+	exclusions = %w(a an and of in the)
+	if exclusions.include? word
+		word
+		else
+		word.capitalize
+	end
+	end
+	@title = split_titles.join(" ")
+	end
+		
+
 end
