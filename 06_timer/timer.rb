9289228a2bda9a5c81ca20623ff28899cb8934bd
tassaddar <tassaddar-90@yandex.ru>
2017-05-11 23:36:32 +0300

    finished all tasks

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,16 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+def initialize 
+	@seconds = 0
+	end 
+	
+	def time_string
+		hour = @seconds/3600
+		min = @seconds%3600
+		sprintf("%02d:%02d:%02d", hour, min/60, min%60)
+		
+		end
+  
 end
